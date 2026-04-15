1de11e8c9a9f92e0efe5a0576814173d03b3f67c
Byshov <byshov1998@gmail.com>
2026-04-15 11:54:10 +0200

    add global colors

--- components/BgImage/BgImage.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import banner from "@/public/banner.webp"
-
-export default function BgImage () {
-    return <Image src={banner} alt="Track image banner" width={1440} height={696}/>
-}
