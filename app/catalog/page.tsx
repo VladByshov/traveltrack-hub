561e1439b795cc045820422b94e14204bfd27474
Byshov <byshov1998@gmail.com>
2026-04-16 16:41:13 +0200

    refactor code

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,7 +1,16 @@
-export default function Catalog() {
+
+import {getAllCampers} from "@/lib/api/camperApi";
+
+export default async function Catalog() {
+    const campersData = await getAllCampers();
+    console.log(campersData);
     return (
         <>
-
+            {campersData.campers.map((item) => {
+                return <p>
+                    {item.name}
+                </p>
+            })}
         </>
     );
 }
