2f7a9db3bd3c27d9e560f8c3906ed5c5d1260e7b
Byshov <byshov1998@gmail.com>
2026-04-17 11:22:27 +0200

    add camperscard

--- components/CampersCard/CampersCard.tsx
+++ components/CampersCard/CampersCard.tsx
@@ -1,8 +1,76 @@
-export default function CampersCard (){
+import Image from "next/image";
+import css from "./CampersCard.module.css"
+import {Camper} from "@/types/camper";
+import {FaStar} from "react-icons/fa";
+import {CiMap} from "react-icons/ci";
+import {BsDiagram3, BsFillCarFrontFill, BsFuelPump} from "react-icons/bs";
+import Button from "@/components/Button/Button";
+import Link from "next/link";
+
+interface CamperCardProps {
+    camper: Camper
+}
+
+export default function CampersCard({camper}: CamperCardProps) {
     return (
-        <li>
-            <h2></h2>
-            <p></p>
+        <li className={css.cardCamper}>
+            <div className={css.imageWrapper}>
+                <Image
+                    src={camper.coverImage}
+                    alt={camper.name}
+                    fill
+                    sizes="100 vw"
+                    className={css.img}
+                />
+            </div>
+
+            <div className={css.content}>
+                <div className={css.header}>
+                    <div className={css.titleRow}>
+                        <h3 className={css.name}>{camper.name}</h3>
+                        <div className={css.priceWrapper}>
+                            <span className={css.price}>€{camper.price}</span>
+                        </div>
+                    </div>
+
+                    <div className={css.infoRow}>
+                        <div className={css.rating}>
+                            <FaStar className={css.starIcon}/>
+                            <span>
+                {camper.rating} ({camper.totalReviews} Reviews)
+              </span>
+                        </div>
+                        <div className={css.location}>
+                            <CiMap className={css.mapIcon}/>
+                            <span>{camper.location}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <p className={css.description}>{camper.description}</p>
+
+                <ul className={css.tags}>
+                    <li className={css.tag}>
+                        <BsFuelPump className={css.tagIcon}/>
+                        {camper.engine}
+                    </li>
+                    <li className={css.tag}>
+                        <BsDiagram3 className={css.tagIcon}/>
+                        {camper.transmission}
+                    </li>
+                    <li className={css.tag}>
+                        <BsFillCarFrontFill className={css.tagIcon}/>
+                        {camper.form}
+                    </li>
+                </ul>
+                <Link href={`/catalog/${camper.id}`}>
+                    <Button
+                        className={css.showMoreButton}
+                        text={"Show more"}
+                        color={"green"}
+                    />
+                </Link>
+            </div>
         </li>
     );
 }
