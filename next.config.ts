f9c534096d234bca888644586303fa6284276d1d
Byshov <byshov1998@gmail.com>
2026-04-19 13:22:33 +0200

    add images to next.config.ts to allow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ac.goit.global',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
   reactCompiler: true,
 };
